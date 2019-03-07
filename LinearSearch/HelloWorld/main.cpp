53032270765fbe543c44819717115a7f07b369d2
chenpengsheng <chenpengsheng@wps.cn>
2019-03-07 09:37:03 +0800

    git测试首次提交

---
+++ LinearSearch/HelloWorld/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include<limits.h>
+
+using namespace std;
+
+
+bool GetMinCommonMultiple(int a, int b, int* common, int* multiple)
+{
+    if(a == b)
+    {
+        return false;
+    }
+
+    if(a > b)
+    {
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+    }
+
+    int num1 = a;
+    int num2 = b;
+
+    int result = 0;
+    while(num1 != 0)
+    {
+        result = num2 % num1;
+        num2 = num1;
+        num1 = result;
+    }
+
+    *common = num2;
+    *multiple = (a * b) / (*common);
+    return true;
+}
+
+int main()
+{
+    int lineNumber = 0;
+    cin >> lineNumber;
+    for(int i = 0; i < lineNumber; ++i)
+    {
+        int n, a, b, k = 0;
+        cin >> n >> a >> b >> k;
+        int common;
+        int multiple;
+        if(!GetMinCommonMultiple(a, b, &common, &multiple))
+        {
+            cout << "Lose";
+        }
+        int totalASolveCount = n / a;
+        int totalBSolveCount = n / b;
+        int totalRepetiveCount = n / multiple;
+
+        if(totalASolveCount + totalBSolveCount - 2 * totalRepetiveCount >=k )
+        {
+            cout << "Win";
+        }
+        else
+        {
+            cout << "Lose";
+        }
+    }
+    return 0;
+}
